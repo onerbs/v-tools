f6b28728dd83d4f0464400e860bf87d507c74f0b
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-02-25 01:13:01 -0600

    rs: Reformat comments

--- src/rs.v
+++ src/rs.v
@@ -7,22 +7,22 @@ import rand
 
 // Create random strings
 
-/*
-Usage: rs <options>
-
-Options:
-  -s <size>  Set the size of the generated string
-
-  -u         Generate the string in uppercase
-  -l         Generate the string in lowercase
-           * By default generate in mixed case
-
-  -a         Use the alpha character set
-  -c         Use the ASCII character set
-  -d         Use the numeric character set
-  -x         Use the hexadecimal character set
-           * By default generate an alphanumeric string
-*/
+//
+// Usage: rs <options>
+//
+// Options:
+//   -s <size>  Set the size of the generated string
+//
+//   -u         Generate the string in uppercase
+//   -l         Generate the string in lowercase
+//            * By default generate in mixed case
+//
+//   -a         Use the alpha character set
+//   -c         Use the ASCII character set
+//   -d         Use the numeric character set
+//   -x         Use the hexadecimal character set
+//            * By default generate an alphanumeric string
+//
 
 fn main() {
 	mut args := xsh.get_args()
