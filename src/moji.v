15d5e4c189b2e0d47c035b6c1de720ca497a193c
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-03-18 05:41:13 -0600

    tool(moji): feat: Ability to find an emoji

--- src/moji.v
+++ src/moji.v
@@ -7,25 +7,46 @@ import moji
 // Usage: moji [<options>] [<input>]
 //
 // Options:
-//   -l    Print the emoji list and exit
-//   -     Read <input> from the standard input
+//   -f <name>    Find an emoji by name
+//   -l           Print the emoji list and exit
+//   -            Read <input> from the standard input
 //
 // Examples:
+//   moji -f heart
 //   moji :sparkles:
 //   git log --oneline | moji -
 //
 
 fn main() {
-	args := plu.need_args(1) or { return }
-	if args == ['-l'] {
-		for key, value in moji.data {
-			emoji := string(value)
-			println('$emoji\t$key')
+	mut args := plu.need_args(1) or { return }
+
+	find := plu.parse_flag(mut args, ['-f'])
+	list := plu.parse_flag(mut args, ['-l'])
+
+	if list {
+		get_emoji('')
+	} else if find {
+		for key in args {
+			get_emoji(key)
 		}
-		return
+	} else {
+		lines := plu.get_lines_from(args)
+		for line in lines {
+			println(moji.parse(line))
+		}
 	}
-	lines := plu.get_lines(args)
-	for line in lines {
-		println(moji.parse(line))
+}
+
+fn get_emoji(key string) {
+	for name, value in moji.data {
+		if key in name {
+			emoji := string(value)
+			if key.len > 0 {
+				found := name.replace_once(key, '\e[1;35m$key\e[m')
+				println('$emoji\t$found')
+			} else {
+				println('$emoji\t$name')
+			}
+		}
 	}
 }
